3f71a447e5b44d329d88d037cb975d9a15e9b76c
ljw <ljw@hds.com>
2019-03-27 16:12:26 +0800

    修改模板

--- src/main/resources/XXXController.php
+++ src/main/resources/XXXController.php
@@ -10,6 +10,8 @@ use App\Models\XXX;
 
 class XXXController extends Controller
 {
+    //nullable可以改为required
+    //integer类型的type和status可以在末尾追加|in:0,1,2进一步限制数值范围
     private $rules = [
        @@@fillrule
     ];
@@ -63,7 +65,7 @@ class XXXController extends Controller
         DB::beginTransaction();
         try{
             for($i = 0; $i < $size; $i++){
-                AdminUser::where('id', '=', $ids[$i])->update(['字段' => $data['字段']]);
+                XXX::where('id', '=', $ids[$i])->update(['字段' => $data['字段']]);
             }
             DB::commit();
             return $this->success();
@@ -85,6 +87,7 @@ class XXXController extends Controller
 
     public function queryById(Request $request, $id)
     {
+        //同$vo = XXX::where('id', '=', $id)->get();
         $vo = XXX::find($id);
         return $this->successWithData($vo);
     }
@@ -92,6 +95,7 @@ class XXXController extends Controller
     //page参数指定当前页,laravel可以自动获取json里和url参数里的page字段值
     public function queryList(Request $request)
     {
+        //获取url参数
         $data = $request->only(['page_size',@@@fillable]);
 
         $validator = Validator::make($data, $this->rules);
@@ -100,12 +104,15 @@ class XXXController extends Controller
             return $this->validateError($validator->errors());
         }
 
-        //select的字段可以通过as重命名来对应接口文档里的字段名,paginate返回的对象可以取出数据重新组织来对应接口文档里的字段名
+        //select的字段可以通过as重命名来对应接口文档里的字段名
+        //paginate返回的对象可以取出数据重新组织来对应接口文档里的字段名
+        //不需要分页时把paginate()改为get()即可
+        //多表关联查询可以套用DB::select('select * from users where gender = ? and city = ?', ['male', 'bj']);
         $vo = XXX::
         select(['id',@@@fillable,'created_at', 'updated_at'])->
         @@@fillwhere
         orderBy('id', 'desc')->
-        paginate(isset($data['page_size']) && is_numeric($data['page_size']) && intval($data['page_size']) > 0 && intval($data['page_size']) < 100 ? $data['page_size'] : 10);
+        paginate(isset($data['page_size']) && is_numeric($data['page_size']) && intval($data['page_size']) > 0 && intval($data['page_size']) < 100 ? intval($data['page_size']) : 10);
         return $this->successWithPage($vo);
     }
 }
